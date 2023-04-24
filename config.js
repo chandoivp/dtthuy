6c4a6a0838c082ec416ed859a0fcd54c7ffc6869
chandoivp <83225017+chandoivp@users.noreply.github.com>
2023-04-24 23:50:26 +0700

    oke
    oke

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Nhóc';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Tặng em';
+const titleCard = 'Tặng nhóc';
+const contentCard = 'Nhân ngày sinh nhật của anh, chúc em luôn vui vẻ, mãi xinh đẹp và dễ thương. Thức dậy và đừng buồn vì bất cứ chuyện gì nhé cô bé. <3';
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
